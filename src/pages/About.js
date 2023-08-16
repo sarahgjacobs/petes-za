274756b99c6de066e485ad34dd5e898d971c9beb
Sarah Jacobs <sgjacobs14@gmail.com>
2023-08-15 21:48:08 -0400

    added pepperoni styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import pizzas from '../assets/brenan-greene-HPZs4EXRFSU-unsplash.jpg'
+import '../styles/About.css'
 
 function About() {
   return (
     <div className='about'>
         <h1>About Pete's Za</h1>
         <div className='aboutTop' style={{ backgroundImage: `url(${pizzas})`}}>
-        <h2>Pete's Za was established in 2023 </h2>
-        <h3>Pete grew up on a pizza farm and made his very first pizza at just 3 years old. Ever since then, it's been his dream to make online pizzas so everyone can enjoy!</h3>
+        
         </div>
             <div className='aboutBottom'>
-
+            <h2>Pete's Za was established in 2023 </h2>
+        <h3>Hey there, pizza enthusiasts! Let us spin you a tale about Pete – a 
+            true farm-to-table champion turned pizza maestro. Born and raised on 
+            a farm, Pete's love for fresh, lip-smacking goodness led him to 
+            create Pete's Za in 2023. With a dash of that farm-fresh charm and a
+             sprinkle of pizza magic, Pete's Za is here to serve you some seriously
+              delicious slices. Every pizza tells a story of Pete's roots and his 
+              commitment to top-notch ingredients. So, join us in savoring the taste 
+              of quality, authenticity, and a sprinkle of Pete's passion in every bite.</h3>
             
         </div>
     </div>
